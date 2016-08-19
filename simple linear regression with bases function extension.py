a71391745c52405492acf21cfc848bca17429618
J3141 <jan.dumke@gmail.com>
2016-08-19 14:43:02 +0200

    bases function extension

--- simple linear regression with bases function extension.py
+++ simple linear regression with bases function extension.py
@@ -1,26 +1,30 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-""" plain simple linear regression """
+""" plain simple linear regression with bases function extensiony """
 
 
 """ Draw x-coorinates uniformly distributed in [a,b]"""
-a, b = -1, 1
-nr_points = 10
+a, b = -4, 1
+nr_points = 20
+
+def bases_function_expansion(x):
+    return np.hstack([x, x**2, x**3, x**4])
 
 def transform_to_intervall(x, low_bound=-1, upper_bound=1):
     return (upper_bound - low_bound) * x + low_bound
 
 x = transform_to_intervall(np.random.random(nr_points),
-                           low_bound=-1, upper_bound=1)
+                           low_bound=a, upper_bound=b)
+
 x.sort()  # for plotting
 
 """ Draw y-coorindates normally distributed with std rho
     and mean conditional_mean"""
-rho = 0.3
+rho = 5
 
 def conditional_mean(x):
-    return x**2 + x + 1
+    return x**3 - x**2 - 10*x + 1
 
 y = np.random.normal(conditional_mean(x), rho, nr_points)
 
@@ -28,17 +32,17 @@ y = np.random.normal(conditional_mean(x), rho, nr_points)
 X = x.reshape(-1, 1)
 Y = y.reshape(-1, 1)
 
-X = np.hstack([X, np.ones_like(X)])     # add biases
+X = np.hstack([bases_function_expansion(X), np.ones_like(X)])     # add biases
 w = np.linalg.inv(X.T @ X) @ X.T  @ Y
 
 """ Prediction """
-x_true = np.linspace(a, b)
+x_true = np.linspace(a, b, 100).reshape(-1, 1)
 y_true = conditional_mean(x_true)
-prediction = (w[0] @ x_true.reshape(1, -1) + w[1])
-
+prediction = (w[:-1].T @ bases_function_expansion(x_true).T + w[-1])
+ 
 """ plot Solution """
 plt.plot(x, y, "o", label="$mu_{Data}$")
 plt.plot(x_true, y_true, "-", label="$mu_{true}$")
-plt.plot(x_true, prediction, "-", label="$mu_{pred.}$")
+plt.plot(x_true, prediction.T, "-", label="$mu_{pred.}$")
 plt.legend()
 plt.title("Linear Regression")
